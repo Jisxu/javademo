050fbbb03d2e438b20945c3206b30d3f121de640
Jisxu <justinfox1206@gmail.com>
2019-11-11 15:02:31 +0800

    update demo

--- src/main/java/top/jisxu/Main.java
+++ src/main/java/top/jisxu/Main.java
@@ -6,10 +6,10 @@ import java.util.stream.Collectors;
 
 public class Main {
     public static void main(String[] args) {
-        String word = "hello world";
+        String word = "祥瑞生辉三羊献瑞三羊生瑞气";
         Map<String, Integer> map = Arrays.stream(word.split(""))
                 .map(String::toUpperCase)
-                .collect(Collectors.toMap(s -> s, s -> 1, (s1, s2) -> s1 + s2));
+                .collect(Collectors.toMap(s -> s, s -> 1, Integer::sum));
         System.out.println(map);
     }
 }
